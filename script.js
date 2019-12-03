29f97740deef8abb82767bed14151def8038cbda
Katherine Mundale <katherine.doris.mundale@gmail.com>
2019-12-03 15:42:31 -0500

    basic html, basic js, and simple css

---
+++ script.js
@@ -0,0 +1,10 @@
+const mileadge = document.querySelector(".mileadge");
+const startScreen = document.querySelector("startScreen");
+const gameArea = document.querySelector("gameArea");
+const deathScreen = document.querySelector("deathScreen");
+
+startScreen.addEventListener("click",start);
+
+function start(){
+  
+}
