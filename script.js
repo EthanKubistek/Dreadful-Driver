2b4b538dcb8e7d4db25fbc3860bb0f4436e6b1b8
Ethan Kubistek <ekubistek@gmail.com>
2019-12-03 15:45:48 -0500

    sectioned js file

---
+++ script.js
@@ -0,0 +1,84 @@
+/* story 1 seeing car and obstacles on the road*/
+
+
+
+
+
+
+
+
+
+
+
+
+/* story 2 making the road and obstacles to scroll*/
+
+
+
+
+
+
+
+
+
+ /* story 3A moving car from left to right with aroow keys*/
+
+
+
+
+
+
+
+
+
+
+
+/* 3B knowing you have hit an obstacle*/
+
+
+
+
+
+
+
+
+
+
+
+/* 4 barrier to keep player on the road*/
+
+
+
+
+
+
+
+
+
+
+
+/* 5 scrolling speed increasing to increaes the difficulty of the game*/
+
+
+
+
+
+
+
+
+
+
+/* 6 mileage counter*/
+
+
+
+
+
+
+
+
+
+
+
+
+/* 7 rest the game*/
