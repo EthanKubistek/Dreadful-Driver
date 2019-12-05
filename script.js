6bb48691b8307109038fd7682caa201e8f29fc16
Katherine Mundale <katherine.doris.mundale@gmail.com>
2019-12-05 13:54:50 -0500

    fixed road, got car to respond to arrow keys, fixed bugs

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 
 /* story 1 seeing car and obstacles on the road*/
 const mileadge = document.querySelector(".mileadge");
-const startScreen = document.querySelector("startScreen");
-const gameArea = document.querySelector("gameArea");
-const deathScreen = document.querySelector("deathScreen");
+const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
+const deathScreen = document.querySelector(".deathScreen");
 
-<<<<<<< HEAD
-const button = document.querySelector("button");
-button.addEventListener("click",start);
-=======
 const button = document.querySelector("Button")
 button.addEventListener("click",start);
 
 
->>>>>>> 1a0c0fa6b86efb8c241c930361de38ae8eb12887
 
 let player = {
   speed: 5
+  ,x:0
+  ,y:0
 };
 let keys = {
-    ArrowRight: false
-        ,ArrowLeft: false
-        
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRight: false,
+    ArrowLeft: false
 };
 
 document.addEventListener("keydown", pressOn);
@@ -30,29 +28,32 @@ document.addEventListener("keyup", pressOff);
 function playGame() {
     let car = document.querySelector(".car");
     let road = gameArea.getBoundingClientRect();
-    console.log(road);
-    console.log(player.x);
     if(player.start) {
-        if (keys.ArrowLeft && player.x > 0) {
+      console.log(road, player)
+        if (keys.ArrowUp && player.y > road.top){
+          player.y -= player.speed;
+        } if (keys.ArrowDown && player.y < road.bottom){
+          player.y += player.speed;
+        } if (keys.ArrowLeft && player.x > 502) {
             player.x -= player.speed;
-        }
-        if (key.ArrowRight && player.x < road.width)
+        } if (keys.ArrowRight && player.x < road.width + 415){
             player.x += player.speed;
     }
-        car.getElementsByClassName.left = player.x + 'px';
+        car.style.left = player.x + "px";
+        car.style.top = player.y + "px";
         window.requestAnimationFrame(playGame);
     }
-
+}
 
 function pressOn(e) {
-    e.perventDefault();
+    e.preventDefault();
     keys[e.key] = true;
     console.log(keys);
 }
 
 
 function pressOff(e) {
-    e.perventDefault();
+    e.preventDefault();
     keys[e.key] = false;
     console.log(keys);
 }
@@ -66,6 +67,8 @@ function start(){
   car.innerText= "car";
   car.setAttribute("class","car");
   gameArea.appendChild(car);
+  player.x = car.offsetLeft;
+  player.y = car.offsetTop;
 }
 
 
