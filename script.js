42c721aa2f166b77610c735c0d5d429e470f2332
Katherine Mundale <katherine.doris.mundale@gmail.com>
2019-12-09 14:28:54 -0500

    added collision detection, death screen, respawn button.

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ const startScreen = document.querySelector(".startScreen");
 const gameArea = document.querySelector(".gameArea");
 const deathScreen = document.querySelector(".deathScreen");
 
-const button = document.querySelector("Button")
+const button = document.querySelector(".start");
 button.addEventListener("click",start);
 
-
+const restart = document.querySelector(".respawn");
+restart.addEventListener("click",reload);
+function reload() {
+  window.location.reload();
+}
 
 let player = {
   speed: 4
@@ -42,16 +46,24 @@ function moveLines() {
     });
   }
 
-  function movePotholes() {
+function movePotholes() {
         let potholes = document.querySelectorAll(".potholes");
         potholes.forEach(function (item) {
-        if (item.y >= 2000) {
-            item.y -= 2000;
+        if (item.y >= 1000) {
+            item.y -= 1000;
         }
         item.y += player.speed;
         item.style.top = item.y + "px";
       });
-  }
+      let car = document.querySelector(".car");
+      potholes.forEach(function (item) {
+              if (isCollide (car, item)) {
+                gameArea.classList.add("hide");
+                deathScreen.classList.remove("hide");
+      }
+  });
+}
+
 
 
 function playGame() {
@@ -73,7 +85,7 @@ function playGame() {
         car.style.top = player.y + "px";
         window.requestAnimationFrame(playGame);
     }
-}
+  }
 
 function pressOn(e) {
     e.preventDefault();
@@ -104,104 +116,25 @@ function start(){
     div.style.top = (x*150) + "px";
     gameArea.appendChild(div);
   }
+  for(let x=0; x<3; x++){
+      let div = document.createElement('div');
+      div.classList.add("potholes");
+      div.y= (x*500);
+      div.style.top = (x*150) + "px";
+      div.style.left = (Math.floor(Math.random()*100)) + "%";
+      gameArea.appendChild(div);
+    }
   window.requestAnimationFrame(playGame);
   let car = document.createElement("div");
   car.setAttribute("class","car");
   gameArea.appendChild(car);
   player.x = car.offsetLeft;
   player.y = car.offsetTop;
-  for(let x=0; x<7; x++){
-      let div = document.createElement('div');
-      div.classList.add("potholes");
-      div.y= (x*500);
-      console.log(div.y);
-      div.style.top = (x*150) + "px";
-      div.style.left = (Math.floor(Math.random()*100)- 12) +"%";
-      gameArea.appendChild(div);
-    }
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* story 2 making the road and obstacles to scroll*/
-
-
-
-
-
-
-
-
-
- /* story 3A moving car from left to right with aroow keys*/
-
-
-
-
-
-
-
-
-
-
-
-/* 3B knowing you have hit an obstacle*/
-
-
-
-
-
-
-
-
-
-
-
-/* 4 barrier to keep player on the road*/
-
-
-
-
-
-
-
-
-
-
-
-/* 5 scrolling speed increasing to increaes the difficulty of the game*/
-
-
-
-
-
-
-
-
-
-
-/* 6 mileage counter*/
-
-
-
-
-
-
-
-
-
-
-
-
-/* 7 rest the game*/
+  function isCollide(a, b) {
+            let aRect = a.getBoundingClientRect();
+            let bRect = b.getBoundingClientRect();
+            return !(
+                (aRect.bottom < bRect.top) || (aRect.top > bRect.bottom) || (aRect.right < bRect.left) || (aRect.left > bRect.right))
+        }
